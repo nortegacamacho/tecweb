7ce84025598ec1f05d2f1be5aa4dd6a9afc8693d
nortegacamacho <nortegacamacho@gmail.com>
2026-08-11 12:30:01 +0000

    1

---
+++ nati/javascript/code/27_control_SELECT.js
@@ -0,0 +1,12 @@
+function cambiarColor()
+{
+    document.form1.text1.value=
+document.form1.select1.selectedIndex;
+    document.form1.text2.value=
+
+document.form1.select1.options[document.form1.select1.selectedIndex].text;
+    document.form1.text3.value=
+        document.form1.select1.options
+    [document.form1.select1.selectedIndex].value;
+}
+
